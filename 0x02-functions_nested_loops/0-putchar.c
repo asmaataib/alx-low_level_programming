f24eb2f51fa655f62ffdbd441827e4bb67cfaae8
asmaataib <asmaataib31@gmail.com>
2023-04-25 11:18:07 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/*
- * This program prints "_putchar" followed by a newline character
- * to the console.
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 	int main(void)
 {
 	printf("_putchar\n");
