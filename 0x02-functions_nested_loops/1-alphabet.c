5fc877fd461449fd7754cca8bbeb6c9ff7087c28
asmaataib <asmaataib31@gmail.com>
2023-04-25 12:07:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c) 	
+	
+	{
+
+	_putchar(c);
+
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
