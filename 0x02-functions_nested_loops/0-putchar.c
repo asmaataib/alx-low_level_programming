3176ea6a7cc275e982e44706d160c5adbd92a05b
asmaataib <asmaataib31@gmail.com>
2023-04-25 13:31:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,9 +2,11 @@
 /**
  * main - Entry point
  * Description: 'prints _putchars to stdout'
- * Return: Always 0 
- */
+ * Return: Always 0
+*/
+
 int main(void)
+
 {
 	_putchar('_');
 	_putchar('p');
@@ -15,5 +17,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+	return (0);
 }
